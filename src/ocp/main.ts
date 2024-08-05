4afbf94c5a170b646b4c13ba2a2ee006f6c0b35f
Dnysss <denysantos2014@hotmail.com>
2024-08-05 14:47:40 -0300

    OCP - OpenClosed Principle

---
+++ src/ocp/main.ts
@@ -0,0 +1,27 @@
+import { Messaging } from "./services/messaging";
+import { Order } from "./classes/order";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/product";
+import { ShoppinCart } from "./classes/shopping-cart";
+import { NoDiscount, TenPercentDiscount } from "./classes/discount";
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppinCart = new ShoppinCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppinCart, messaging, persistency);
+
+shoppinCart.addItem(new Product("Camiseta", 49.9));
+shoppinCart.addItem(new Product("Caderno", 50.9));
+shoppinCart.addItem(new Product("Lapis", 1.5));
+
+console.log(shoppinCart.item);
+console.log(shoppinCart.total());
+console.log(shoppinCart.totalWithDiscount());
+console.log(order.orderStatus);
+
+order.checkout();
+
+console.log(order.orderStatus);
